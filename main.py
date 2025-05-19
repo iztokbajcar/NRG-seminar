d20899f56851179139fc163f2875bc4ef97c2881
Iztok Bajcar <iztok.bajcar@gmail.com>
2025-05-19 14:37:32 +0200

    Added a code cell in main.py that plots a coreset

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # %%
 from src.point_cloud import PointCloud
 from src.sensitivity_sampling import SensitivitySampling
+from matplotlib import pyplot as plt
 import numpy as np
 import os
 
@@ -13,7 +14,25 @@ print(f"{len(pc.points_x)} points")
 ss = SensitivitySampling(pc)
 
 # %%
-print(ss.sample(10))
+coreset, weights = ss.sample(100)
+
+# %%
+print(f"coreset: {coreset}")
+print(f"weights: {weights}")
+
+# %%
+coreset_x = [p[0] for p in coreset]
+coreset_y = [p[1] for p in coreset]
+coreset_z = [p[2] for p in coreset]
+
+# %%
+# plot
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+ax.scatter(coreset_x, coreset_y, coreset_z, cmap="viridis")
+fig.show()
+
 
 # %%
 print(pc.get_labels())
